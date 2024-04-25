f8402b4ecc141c1bdd94d69c33f7a2a207d0142c
Kéwan Bernier <mail@kewan.fr>
2024-04-25 19:50:52 +0200

    types ITrackDetails simple object to avoid creating ITrack obj

--- src/lib/details/Track.ts
+++ src/lib/details/Track.ts
@@ -6,6 +6,18 @@ export default class TrackDetails implements ITrack {
         public artists: string[] = [],
         public album_name = '',
         public release_date = '',
-        public cover_url = ''
+        public cover_url = '',
+        public track_number = 0,
+        public youtube_url: string | null = null
     ) {}
+}
+
+export interface ITrackDetails {
+    name: string
+    artists: string[]
+    album_name: string
+    release_date: string
+    cover_url: string
+    track_number: number,
+    youtube_url?: string | null,
 }
